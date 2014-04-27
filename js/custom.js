626dbe160a5779c303932266de60aa2489071dfa
Alberto Villalobos <alberto@albertovillalobos.me>
2014-04-27 14:08:48 -0700

    initial commmit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// console.log('\'Allo \'Allo!');
+(function($) {
+    'use strict';
+
+    // $(document).scroll(function(event) {
+    //     var newHeight = ($(window).scrollTop() / $(document).height()) * 1200 * (-1) + 500;
+    //     console.log(newHeight);
+    //     $("section.background1").css("background-position-y", newHeight + "px");
+    // });
+})(jQuery);
